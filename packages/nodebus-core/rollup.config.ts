807580d34f67883f0cc24c59df01ee51293bf94f
Arslan <rslnkrmt2552@gmail.com>
2022-08-20 21:24:41 +0500

    update build config to emit declaration files

--- packages/nodebus-core/rollup.config.ts
+++ packages/nodebus-core/rollup.config.ts
@@ -1,20 +1,20 @@
 import pkg from './package.json';
 import { swc, defineRollupSwcOption } from 'rollup-plugin-swc3';
 import type { RollupOptions } from 'rollup';
-
+//todo: maybe use "rollup-plugin-dts" for d.ts
 const config: RollupOptions =
     // CommonJS (for Node) & ESM (for bundlers)
     {
         input: 'src/index.ts',
         output: [
-            { file: pkg.main, format: 'cjs' },
-            { file: pkg.module, format: 'es' },
+            { file: pkg.main, format: 'cjs', sourcemap: false },
+            { file: pkg.module, format: 'es', sourcemap: false },
         ],
         plugins: [
             swc(
                 defineRollupSwcOption({
                     minify: true,
-                    sourceMaps: true,
+                    tsconfig: './tsconfig.build.json'
                 }),
             ),
         ],
