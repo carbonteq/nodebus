e080e99968c719b9f7577045630a7034201af792
Arslan <rslnkrmt2552@gmail.com>
2022-08-22 17:42:30 +0500

    feat: add local bus impl

---
+++ packages/core/src/local.bus.ts
@@ -0,0 +1,47 @@
+import * as events from 'node:events';
+import {
+  IClassHandler,
+  IHandlerRegistry,
+  IMessage,
+  IMessageBus,
+  // ISerializable,
+  // ISerializer,
+} from 'src/base';
+import { DefaultHandlerRegistry } from './default.handler-registry';
+// import { JSONSerializer } from './json.serializer';
+
+export class LocalBus implements IMessageBus {
+  // warn: unused
+  // private readonly serializer: ISerializer<ISerializable>;
+  private readonly handlerRegistry: IHandlerRegistry;
+  // fixme: why static?
+  private static readonly eventEmitter = new events.EventEmitter();
+
+  constructor() {
+    // this.serializer = new JSONSerializer();
+    this.handlerRegistry = new DefaultHandlerRegistry();
+  }
+
+  // warn: this requires the bus to be started either explicitly,
+  // or we have to ensure that all the required events are registered before the bus starts
+  async start(): Promise<void> {
+    const listenerMap = this.handlerRegistry.getAll();
+
+    listenerMap.forEach((handlers, eventName) => {
+      handlers.forEach((handler) => {
+        LocalBus.eventEmitter.on(eventName, handler.handle);
+      });
+    });
+  }
+
+  async publish(event: IMessage): Promise<IMessage> {
+    // note: maybe serializer should be used here
+    LocalBus.eventEmitter.emit(event.name, event);
+
+    return event;
+  }
+
+  subscribe<T extends IMessage>(handler: IClassHandler<T>): void {
+    this.handlerRegistry.register(handler.eventType, handler);
+  }
+}
