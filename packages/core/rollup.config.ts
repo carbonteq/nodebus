e7721a620d02792c63e862acf8e3629f38522345
Arslan <rslnkrmt2552@gmail.com>
2022-08-23 19:00:57 +0500

    update formatting for rollup config

--- packages/core/rollup.config.ts
+++ packages/core/rollup.config.ts
@@ -3,21 +3,22 @@ import { swc, defineRollupSwcOption } from 'rollup-plugin-swc3';
 import type { RollupOptions } from 'rollup';
 //todo: maybe use "rollup-plugin-dts" for d.ts
 const config: RollupOptions =
-    // CommonJS (for Node) & ESM (for bundlers)
-    {
-        input: 'src/index.ts',
-        output: [
-            { file: pkg.main, format: 'cjs', sourcemap: false },
-            { file: pkg.module, format: 'es', sourcemap: false },
-        ],
-        plugins: [
-            swc(
-                defineRollupSwcOption({
-                    minify: true,
-                    tsconfig: './tsconfig.build.json'
-                }),
-            ),
-        ],
-    };
+  // CommonJS (for Node) & ESM (for bundlers)
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: false },
+      { file: pkg.module, format: 'es', sourcemap: false },
+    ],
+    plugins: [
+      swc(
+        defineRollupSwcOption({
+          minify: true,
+          tsconfig: './tsconfig.build.json',
+        }),
+      ),
+    ],
+    external: ['node:events', 'regenerator-runtime'],
+  };
 
 export default config;
