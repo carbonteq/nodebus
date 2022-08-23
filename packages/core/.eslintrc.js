5dd1645d31dbe075eefd0c9902574d0f852b5c50
Arslan <rslnkrmt2552@gmail.com>
2022-08-24 02:43:28 +0500

    use shared eslint config, and pin deps in packages

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,32 +1,3 @@
-const config = {
-  // "$schema": "https://json.schemastore.org/eslintrc.json",
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  env: {
-    browser: false,
-    node: true,
-    es2021: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc', 'node_modules'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { ignoreRestSiblings: true },
-    ],
-    '@typescript-eslint/no-empty-function': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-  },
-};
+const defaultConfig = require('eslint-config-nodebus');
 
-module.exports = config;
+module.exports = defaultConfig;
---
+++ packages/eslint-config-nodebus/index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /* "$schema": "https://json.schemastore.org/eslintrc.json", */
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {},
+  env: {
+    browser: false,
+    node: true,
+    /* es2021: true, */
+    jest: true,
+  },
+  ignorePatterns: [
+    ".eslintrc",
+    "node_modules",
+    ".eslintrc.js",
+    ".eslintrc.json",
+    ".prettierrc",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { ignoreRestSiblings: true },
+    ],
+    "@typescript-eslint/no-empty-function": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+  },
+};
