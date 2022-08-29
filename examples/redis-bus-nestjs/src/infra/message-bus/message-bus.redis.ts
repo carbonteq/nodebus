2ab176875a66506697c9489228443ba65c335b41
Arslan <rslnkrmt2552@gmail.com>
2022-08-29 21:10:58 +0500

    update redis example to use cfg object

--- examples/redis-bus-nestjs/src/infra/message-bus/message-bus.redis.ts
+++ examples/redis-bus-nestjs/src/infra/message-bus/message-bus.redis.ts
@@ -1,18 +1,28 @@
 import { Bus, BusBuilder, PinoLogger } from "@carbonteq/nodebus-core";
 import { Provider } from "@nestjs/common";
 import { FooEventHandler } from "@src/app/handlers/foo.handler";
-import {default as Redis} from 'ioredis'
-import {RedisTransport} from "@carbonteq/nodebus-transport-redis";
+import { default as Redis } from "ioredis";
+import {
+  RedisTransport,
+  RedisTransportConfig
+} from "@carbonteq/nodebus-transport-redis";
 
 export const MemoryBusProvider: Provider<Bus> = {
   provide: Bus,
   useFactory: async () => {
-    const redisClient = new Redis()
-    const redisTransport  = new RedisTransport(redisClient, new PinoLogger())
-    await redisTransport.initialize()
+    const redisClient = new Redis();
+    const pinoLogger = new PinoLogger();
+
+    const cfg: RedisTransportConfig = {
+      client: redisClient,
+      logger: pinoLogger
+    };
+
+    const redisTransport = new RedisTransport(cfg);
+    await redisTransport.initialize();
 
     const bus = await BusBuilder.configure()
-        .withTransport(redisTransport)
+      .withTransport(redisTransport)
       // you can also mark FooEventHandler as Injectable if you want a singleton with some specific config etc
       // and then use https://docs.nestjs.com/fundamentals/custom-providers#factory-providers-usefactory to inject it here
       .addHandler(new FooEventHandler())
