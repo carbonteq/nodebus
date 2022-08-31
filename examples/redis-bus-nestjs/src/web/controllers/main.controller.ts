08c096ce55dcd8fa9c4ad5918ce5a4484ae49e02
Arslan <rslnkrmt2552@gmail.com>
2022-08-31 22:56:53 +0500

    add customId route to redis + nestjs example

--- examples/redis-bus-nestjs/src/web/controllers/main.controller.ts
+++ examples/redis-bus-nestjs/src/web/controllers/main.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 import { BarService } from "@src/app/bar.service";
 
 @Controller("/test")
 export class MainController {
-  // private readonly logger: Logger = new Logger(MainController.name);
-
   constructor(private readonly serv: BarService) {}
 
   @Get()
   async test() {
-    await this.serv.doSomething("some random Id");
+    const id = "nestjs redis"
 
-    return { status: "ok" };
+    await this.serv.doSomething(id);
+
+    return { id };
+  }
+
+  @Get("/:id")
+  async testWithSpecificId(@Param("id") id: string){
+    await this.serv.doSomething(id)
+
+    return {id}
   }
 }
