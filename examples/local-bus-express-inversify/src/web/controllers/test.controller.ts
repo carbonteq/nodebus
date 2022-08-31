e5d2c6cf62ded4ba796f30e647bdc7b2f2da3175
Arslan <rslnkrmt2552@gmail.com>
2022-09-01 01:26:50 +0500

    await for the service in the controllers

--- examples/local-bus-express-inversify/src/web/controllers/test.controller.ts
+++ examples/local-bus-express-inversify/src/web/controllers/test.controller.ts
@@ -16,14 +16,14 @@ export class TestController implements interfaces.Controller {
   async testEventDefaultId() {
     const id = 'some random id';
 
-    this.serv.doSomething(id);
+    await this.serv.doSomething(id);
 
     return { id };
   }
 
   @httpGet('/:id')
   async testEventWithId(@requestParam('id') id: string) {
-    this.serv.doSomething(id);
+    await this.serv.doSomething(id);
 
     return { id };
   }
